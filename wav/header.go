5fa06141f5a684c1b8c1429fe6c0d37e45c84252
mewmew <rnd0x00@gmail.com>
2014-08-09 03:29:26 +0200

    wav: Initial implementation of a wav audio encoder.

---
+++ wav/header.go
@@ -0,0 +1,101 @@
+package wav
+
+import (
+	"encoding/binary"
+	"io"
+
+	"azul3d.org/audio.v1"
+)
+
+// writeHeader writes a WAV file header to w, based on the provided audio
+// configuration.
+func writeHeader(w io.Writer, conf audio.Config) error {
+	// placeholder is used when a value of the WAV header cannot be determined in
+	// advance. After the last audio sample has been encoded these placeholder
+	// values must be updated, which is why an io.WriteSeeker is required.
+	const placeholder = 0xED0CDAED
+
+	// RIFF type chunk.
+	riff := riff{
+		typ: 0x57415645, // "WAVE"
+	}
+	riff.id = 0x52494646 // "RIFF"
+	riff.size = placeholder
+	err := binary.Write(w, binary.LittleEndian, riff)
+	if err != nil {
+		return err
+	}
+
+	// WAVE format chunk.
+	format := format{
+		format:     formatPCM,
+		nchannels:  uint16(conf.Channels),
+		sampleRate: uint32(conf.SampleRate),
+		byteRate:   uint32(conf.Channels * conf.SampleRate * bps / 8),
+		blockAlign: uint16(conf.Channels * bps / 8),
+		bps:        bps,
+	}
+	format.id = 0x666D7420 // "fmt "
+	format.size = 16
+	err = binary.Write(w, binary.LittleEndian, format)
+	if err != nil {
+		return err
+	}
+
+	// WAVE data chunk.
+	data := chunkHeader{
+		id:   0x64617461, // "data"
+		size: placeholder,
+	}
+	err = binary.Write(w, binary.LittleEndian, data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// chunkHeader specifies the size and id of a chunk.
+type chunkHeader struct {
+	// The contents of the chunk body is derived from its id.
+	id uint32
+	// The size in bytes of the chunk body.
+	size uint32
+}
+
+// riff represents a RIFF type chunk.
+type riff struct {
+	// Chunk header
+	//    id:   "RIFF"
+	//    size: 0004
+	chunkHeader
+	// RIFF type: "WAVE".
+	typ uint32
+}
+
+// format describes the format of the audio stream.
+type format struct {
+	// Chunk header
+	//    id:   "fmt "
+	//    size: 0016
+	chunkHeader
+	// Audio format.
+	//    1 = PCM format.
+	format uint16
+	// Number of channels.
+	nchannels uint16
+	// Samples rate.
+	sampleRate uint32
+	// Average number of bytes per second.
+	//    (nchannels * sampleRate * bps / 8)
+	byteRate uint32
+	// Block alignment.
+	//    (nchannels * bps / 8)
+	blockAlign uint16
+	// Sample size in bits-per-sample.
+	bps uint16
+}
+
+// formatPCM specifies that the audio samples are stored uncompressed, using
+// pulse-code modulation.
+const formatPCM = 1
---
+++ wav/wav.go
@@ -0,0 +1,75 @@
+// Package wav provides a WAV audio encoder.
+//
+// A brief introduction of the WAV audio format [1][2] follows. A WAV file
+// consists of a sequence of chunks as specified by the RIFF format. Each chunk
+// has a header and a body. The header specifies the type of the chunk and the
+// size of its body.
+//
+// The first chunk of a WAV file is the standard RIFF type chunk, with a "WAVE"
+// type ID. It is followed by a mandatory format chunk, which describes the
+// basic properties of the audio stream; such as its sample rate and the number
+// of channels used. Subsequent chunks may appear in any order and several
+// chunks are optional. The only other chunk that is mandatory is the data
+// chunk, which contains the encoded audio samples.
+//
+// Below follows an overview of a basic WAV file.
+//
+//    Header: {id: "RIFF", size: 0004}
+//    Body:   "WAVE"
+//    Header: {id: "fmt ", size: NNNN}
+//    Body:   format of the audio samples
+//    Header: {id: "data", size: NNNN}
+//    Body:   audio samples
+//
+// Please refer to the WAV specification for more in-depth details about its
+// file format.
+//
+//    [1]: http://www.sonicspot.com/guide/wavefiles.html
+//    [2]: https://ccrma.stanford.edu/courses/422/projects/WaveFormat/
+package wav
+
+import (
+	"io"
+
+	"azul3d.org/audio.v1"
+)
+
+// encoder is capable of encoding audio samples to a WAV file.
+type encoder struct {
+	// Underlying io.WriteSeeker to which the WAV file is written to.
+	w io.WriteSeeker
+	// Audio configuration; including sample rate and number of channels.
+	conf audio.Config
+}
+
+// bps represents the number of bits-per-sample used to encode audio samples.
+const bps = 16
+
+// NewEncoder creates a new WAV encoder, which stores the audio configuration in
+// a WAV header and encodes any audio samples written to it. The contents of the
+// WAV header and the encoded audio samples are written to w.
+func NewEncoder(w io.WriteSeeker, conf audio.Config) (enc audio.Writer, err error) {
+	// Write WAV file header to w, based on the audio configuration.
+	err = writeHeader(w, conf)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return encoder which encodes the audio samples written to it and stores
+	// writes those to w.
+	return &encoder{w: w, conf: conf}, nil
+}
+
+// Write attempts to write all, b.Len(), samples in the slice to the
+// writer.
+//
+// Returned is the number of samples from the slice that where wrote to
+// the writer, and an error if any occured.
+//
+// If the number of samples wrote is less than buf.Len() then the returned
+// error must be non-nil. If any error occurs it should be considered fatal
+// with regards to the writer: no more data can be subsequently wrote after
+// an error.
+func (enc *encoder) Write(b audio.Slice) (n int, err error) {
+	panic("not yet implemented.")
+}
